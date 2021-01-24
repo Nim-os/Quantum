b27c51ef37e6c8c743fe23d18efe19f70aeb0659
Christophe Simon <10841304+Nim-os@users.noreply.github.com>
2021-01-23 19:57:10 -0500

    Add user and reminder

---
+++ User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace mastodonte_bot
+{
+	public class User
+	{
+		public SteamKit2.SteamID userID;
+
+		public Reminder? reminder;
+	}
+
+
+	public struct Reminder
+	{
+		public ulong remindAt;
+		public string message;
+	}
+}
